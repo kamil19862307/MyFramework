1ac5f0848f780539551ecfb927f50e20ce07801e
kamil19862307 <kamil19862307@gmail.com>
2025-08-29 13:34:23 +0300

    feat (CLI) add Summator class to autoload

--- bin/cli.php
+++ bin/cli.php
@@ -1,18 +1,35 @@
 <?php
+try {
+    unset($argv[0]);
 
-unset($argv[0]);
+    // Автозагрузка
+    require __DIR__ . '/../vendor/autoload.php';
 
-$params = [];
+    // Составляем полное имя класса, добавив нэймспейс
+    $className = '\\MyProject\\Cli\\' . array_shift($argv);
 
-foreach ($argv as $argument) {
-    preg_match('/^-(.+)=(.+)$/', $argument, $matches);
-
-    if (!empty($matches)) {
-        $paramName = $matches[1];
-        $paramValue = $matches[2];
-
-        $params[$paramName] = $paramValue;
+    if (!class_exists($className)) {
+        throw new \MyProject\Exceptions\CliException('Class "' . $className . '" not found');
     }
-}
 
-var_dump($params);
+    $params = [];
+
+    foreach ($argv as $argument) {
+        preg_match('/^-(.+)=(.+)$/', $argument, $matches);
+
+        if (!empty($matches)) {
+            $paramName = $matches[1];
+            $paramValue = $matches[2];
+
+            $params[$paramName] = $paramValue;
+        }
+    }
+
+    // Создаём экземпляр класса, передав параметры и вызываем метод execute()
+    $class = new $className($params);
+
+    $class->execute();
+
+} catch (\MyProject\Exceptions\CliException $exception) {
+    echo 'Error ' . $exception->getMessage();
+}
